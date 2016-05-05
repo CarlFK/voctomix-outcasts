8e4fd4776ba03a2ad464191ba04ac2cf27430eea
Carl Karsten <carl@personnelware.com>
2016-05-05 17:19:45 -0500

    fix command names in test scripts.

--- tests/test1.sh
+++ tests/test1.sh
@@ -3,10 +3,11 @@
 # Mostly to confirm the basics is installed and setup properly. 
 cd ../../voctomix
 
-./voctocore/voctocore.py -i ../vocto-outcasts/configs/light.ini & \
-    sleep 5
+./voctocore/voctocore.py -i ../voctomix-outcasts/configs/light.ini &
+sleep 5
 ./voctogui/voctogui.py &
 ../voctomix-outcasts/ingest.py &
+sleep 1
 ../voctomix-outcasts/ingest.py --port 10001 &
 ./example-scripts/gstreamer/record-mixed-av.sh &
 ./example-scripts/control-server/generate-cut-list.py
--- tests/test2.sh
+++ tests/test2.sh
@@ -3,10 +3,11 @@
 # Mostly to confirm the basics is installed and setup properly. 
 # test1.sh expects git checkouts, test2 installed on $PATH
 
-voctocore -i /etc/vocto/light.ini & \
-    sleep 5; \
-voctogui.py &
-ingest.py &
-ingest.py --port 10001 &
-record-mixed-av.sh &
-generate-cut-list.py
+voctocore -i /etc/voctomix/light.ini &
+sleep 5
+voctogui &
+ingest &
+sleep 1
+ingest --port 10001 &
+record-mixed-av &
+generate-cut-list
