bafa6bbb49b433dcbe54e41dbe726b773a822e4e
Ryan Verner <ryan.verner@gmail.com>
2016-09-06 01:14:31 +1000

    Replace 127.0.0.1 with $VOC_CORE

--- record-timestamp.sh
+++ record-timestamp.sh
@@ -8,7 +8,7 @@ segment_time=1800
 mkdir -p $dest_dir/$HOSTNAME/$(date +%Y-%m-%d)
 
 ffmpeg \
-    -i tcp://127.0.0.1:11000 \
+    -i tcp://$VOC_CORE:11000 \
     -ac 2 -channel_layout 2c -aspect 16:9 \
         -map 0:v -c:v:0 mpeg2video -pix_fmt:v:0 yuv422p -qscale:v:0 2 -qmin:v:0 2 -qmax:v:0 10 -keyint_min 0 -bf:0 0 -g:0 0 -intra:0 -maxrate:0 140M \
         -map 0:a -c:a:0 mp2 -b:a:0 192k -ac:a:0 2 -ar:a:0 48000 \
