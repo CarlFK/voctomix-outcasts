31920a8af45ae6daaddb2bb1d91b76da89f1663e
Carl Karsten <carl@personnelware.com>
2016-09-05 22:16:48 -0500

    use localhost for file save sink scripts.

--- record-mixed-av.sh
+++ record-mixed-av.sh
@@ -9,7 +9,7 @@ dest_dir=${1:-~/Videos}/$(date +%Y-%m-%d)
 mkdir -p $dest_dir
 
 gst-launch-1.0 \
-    tcpclientsrc host=127.0.0.1 port=4954 !\
+    tcpclientsrc host=localhost port=4954 !\
     \
     matroskademux name=demux \
     \
