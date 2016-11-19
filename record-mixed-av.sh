ff7160f3c6aa381b721a98920028dacf7284344e
Holger Levsen <holger@layer-acht.org>
2016-11-19 16:32:34 +0100

    add MIT licence information into every relevant file and update LICENSE accordingly. (Carl said he didnt care and debian/copyright said MIT so I choose that)
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- record-mixed-av.sh
+++ record-mixed-av.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+# Copyright: 2015,2016    Carl F. Karsten <carl@nextdayvideo.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 
 # $1 - destination dir. default: ~/Videos 
 # files will be $dest_dir/$date/$time.gs.ts
