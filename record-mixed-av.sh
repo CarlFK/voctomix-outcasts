ef8a67a5a195a2bfbef3a7aaab81cecbb9282680
Tim 'mithro' Ansell <mithro@mithis.com>
2016-12-01 22:26:27 +1100

    Fix record-mixed-av to connect to the right port.

--- record-mixed-av.sh
+++ record-mixed-av.sh
@@ -20,7 +20,7 @@ dest_dir=${1:-~/Videos}/$(date +%Y-%m-%d)
 mkdir -p $dest_dir
 
 gst-launch-1.0 \
-    tcpclientsrc host=localhost port=4954 !\
+    tcpclientsrc host=localhost port=11000 !\
     \
     matroskademux name=demux \
     \
