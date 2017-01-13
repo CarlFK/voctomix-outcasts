888541b12d3e801fb8b159bd3b0b1c1156cb9caf
Stefano Rivera <stefano@rivera.za.net>
2017-01-13 16:20:19 +1100

    Wrap long lines

--- ingest.py
+++ ingest.py
@@ -2,15 +2,16 @@
 """
 ingest.py: source client for Voctomix.
 
-   Copyright: 2015,2016    Carl F. Karsten <carl@nextdayvideo.com>, Ryan Verner <ryan@nextdayvideo.com.au>
+   Copyright: 2015,2016    Carl F. Karsten <carl@nextdayvideo.com>,
+                           Ryan Verner <ryan@nextdayvideo.com.au>
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
-   of this software and associated documentation files (the "Software"), to deal
-   in the Software without restriction, including without limitation the rights
-   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-   copies of the Software, and to permit persons to whom the Software is
+   of this software and associated documentation files (the "Software"), to
+   deal in the Software without restriction, including without limitation the
+   rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+   sell copies of the Software, and to permit persons to whom the Software is
    furnished to do so, subject to the following conditions:
- 
+
    The above copyright notice and this permission notice shall be included in
    all copies or substantial portions of the Software.
 
