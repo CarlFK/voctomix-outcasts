e9108b5fce98d50badc37884a8d52143abe35ecb
Carl Karsten <carl@personnelware.com>
2017-01-26 10:30:14 -0600

    test1 shows ingest getting parameters from server config.

--- tests/test1.sh
+++ tests/test1.sh
@@ -3,11 +3,13 @@
 # Mostly to confirm the basics is installed and setup properly.
 cd ../../voctomix
 
-./voctocore/voctocore.py -i ../voctomix-outcasts/configs/light.ini &
+./voctocore/voctocore.py -i ../voctomix-outcasts/configs/test1.ini &
 sleep 5
 ./voctogui/voctogui.py &
 ../voctomix-outcasts/ingest.py &
-../voctomix-outcasts/ingest.py --source-id grabber &
+../voctomix-outcasts/ingest.py --source-id cam2 &
+../voctomix-outcasts/ingest.py \
+    --video-attribs "pattern=ball" --source-id grabber &
 ../voctomix-outcasts/record-timestamp.sh &
-../voctomix-outcasts/generate-cut-list.py --file foo.log
+../voctomix-outcasts/generate-cut-list.py --file test1.log
 
