533ca42ec8e29472d26268a7b0142e940c02d6ad
Carl Karsten <carl@personnelware.com>
2020-07-31 13:36:42 -0500

    simpler show_hdmi2usb.sh

--- tests/show_hdmi2usb.sh
+++ tests/show_hdmi2usb.sh
@@ -2,14 +2,5 @@
 
 gst-launch-1.0 \
     -v \
-    v4l2src device=/dev/$1 !\
-    decodebin !\
-     queue ! videoconvert !\
-     fpsdisplaysink sync=false
+     v4l2src device=$1 ! jpegdec ! autovideosink sync=false
 
-    # video/raw,width=1280,height=720 !\
-    # video/raw,width=1280,height=960 !\
-     # jpegdec !\
-    # decodebin !\
-    # video/x-raw, format=(string)YUY2, width=(int)1920, height=(int)1080, pixel-aspect-ratio=(fraction)1/1, interlace-mode=(string)progressive, colorimetry=(string)2:4:7:1, framerate=(fraction)5/1'
-    # video/x-raw, format=YUY2, width=1920, height=1080, pixel-aspect-ratio=1/1, interlace-mode=progressive, colorimetry=2:4:7:1, framerate=5/1 !\
