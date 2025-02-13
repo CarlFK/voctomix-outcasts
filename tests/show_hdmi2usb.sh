326698d92b071d2b17a01473d018720d36c2bc2d
Carl Karsten <carl@personnelware.com>
2025-02-12 23:30:24 -0600

    show_hdmi2usb a bit more general-purpose.

--- tests/show_hdmi2usb.sh
+++ tests/show_hdmi2usb.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -x
 
+dev=${1:-/dev/video0}
+
 gst-launch-1.0 \
     -v \
-     v4l2src device=$1 ! jpegdec ! autovideosink sync=false
+     v4l2src device=${dev} ! jpegparse ! decodebin3 ! \
+     autovideosink sync=false
 
+     # v4l2src device=${dev} ! jpegparse ! vaapidecodebin ! \
