950922ec57f75a998e447aa670816e2f317cb014
Carl Karsten <carl@personnelware.com>
2026-04-29 02:52:25 -0500

    cmds - socket.recv response from core.

--- voctocore-cmds.py
+++ voctocore-cmds.py
@@ -25,20 +25,59 @@ import socket
 import sys
 import time
 
-def connect(host='localhost', port=9999):
+
+def connect(host='localhost', port=9999, timeout=2):
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
 
     try:
         sock.connect((host, port))
-        sock.settimeout(2)
+        sock.settimeout(timeout)
     except ConnectionRefusedError:
         sys.exit('ConnectionRefusedError - Voctocore not running?  Exiting bye.')
 
-    fd = sock.makefile('w')
+    return sock
 
-    return fd
+
+def vocto_io(sock, command: bytes):
+    """Send a command to the voctocore control server
+    returnr response.
+
+    s: socket
+    command: voctocore command server command
+    """
+    try:
+        sock.sendall(command)
+        data=sock.recv(10000)
+
+    except socket.timeout as err:
+        sys.exit(f'socket.timeout - There was a problem while sending {command} voctocore.'
+                 'Giving up, bye.')
+
+    return data
+
+
+
+def send_cmds(sock, cmds, delay, verbose):
+    """Send a list of commands
+    delay between each
+    maybe echo before sending
+    print response
+    """
+
+    for cmd in cmds:
+        if verbose: print( f'sending: {cmd}' )
+
+        cmd+="\n"
+        cmd=cmd.encode()
+
+        reply = vocto_io(sock, cmd)
+
+        reply = reply.decode()
+        print(reply)
+
+        time.sleep(delay)
 
 
 def read_cmds(filename):
@@ -50,33 +89,11 @@ def read_cmds(filename):
         cmds = []
         for line in lines:
             line = line.strip()
+            # ignore blank lines.
             if line:
                 cmds.append(line)
 
     return cmds
-
-
-def vocto_send(fd, command: str):
-    """Write commands to the voctocore control server
-
-    fd: socket file object
-    command: voctocore command server command
-    """
-    try:
-        fd.write(command + '\n')
-        fd.flush()
-
-    except socket.timeout as err:
-        sys.exit(f'socket.timeout - There was a problem while sending {command} voctocore.'
-                 'Giving up, bye.')
-
-
-def send_cmds(fd, cmds, delay, verbose):
-
-    for cmd in cmds:
-        if verbose: print( f'sending command: {cmd}' )
-        vocto_send(fd, cmd)
-        time.sleep(delay)
 
 
 def get_args():
@@ -108,6 +125,10 @@ def get_args():
         help="port of vocto core")
 
     parser.add_argument(
+        '--timeout', action='store', type=float, default=.5,
+        help="seconds to wait for reply from server, then abort")
+
+    parser.add_argument(
         '--delay', action='store', type=float, default=0,
         help="delay in seconds between commands")
 
@@ -123,15 +144,17 @@ def get_args():
 def main():
 
     args = get_args()
-    core_ip = socket.gethostbyname(args.host)
-
-    fd=connect(args.host, args.port)
 
     cmds = args.cmds
     if args.file:
         cmds.extend(read_cmds(args.file))
 
-    send_cmds(fd, cmds, args.delay, args.verbose)
+    core_ip = socket.gethostbyname(args.host)
+    if args.verbose:
+        print(f"{core_ip=}")
+
+    with connect(core_ip, args.port, args.timeout) as sock:
+        send_cmds(sock, cmds, args.delay, args.verbose)
 
     sys.exit()
 
