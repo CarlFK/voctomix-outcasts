4a0e878c9564615b411afec21efb8daff473df46
Carl Karsten <carl@personnelware.com>
2026-06-28 01:59:43 -0500

    uncompromising code format.

--- voctocore-cmds.py
+++ voctocore-cmds.py
@@ -28,7 +28,8 @@ import time
 
 logger = logging.getLogger(__name__)
 
-def connect(host='localhost', port=9999, timeout=2, wait=False):
+
+def connect(host="localhost", port=9999, timeout=2, wait=False):
 
     logger.debug(f"Establishing Connection to {host=}")
 
@@ -38,8 +39,8 @@ def connect(host='localhost', port=9999, timeout=2, wait=False):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
 
-    fails=0
-    sleepy_time=2
+    fails = 0
+    sleepy_time = 2
 
     while True:
         try:
@@ -48,12 +49,16 @@ def connect(host='localhost', port=9999, timeout=2, wait=False):
             break
         except ConnectionRefusedError:
             if wait:
-                fails+=1
-                logger.debug(f"ConnectionRefusedError - {fails=} {sleepy_time=} before looping...")
+                fails += 1
+                logger.debug(
+                    f"ConnectionRefusedError - {fails=} {sleepy_time=} before looping..."
+                )
                 time.sleep(sleepy_time)
                 continue
             else:
-                sys.exit('ConnectionRefusedError - Voctocore not running?  Exiting bye.')
+                sys.exit(
+                    "ConnectionRefusedError - Voctocore not running?  Exiting bye."
+                )
 
     if fails:
         logger.debug(f"{fails=} so sleep a little more just to be sure.")
@@ -74,14 +79,15 @@ def vocto_io(sock, command: bytes):
     """
     try:
         sock.sendall(command)
-        data=sock.recv(10000)
+        data = sock.recv(10000)
 
     except socket.timeout as err:
-        sys.exit(f'socket.timeout - There was a problem while sending {command} voctocore.'
-                 'Giving up, bye.')
+        sys.exit(
+            f"socket.timeout - There was a problem while sending {command} voctocore."
+            "Giving up, bye."
+        )
 
     return data
-
 
 
 def send_cmds(sock, cmds, delay):
@@ -92,10 +98,10 @@ def send_cmds(sock, cmds, delay):
     """
 
     for cmd in cmds:
-        logger.info( f'sending: {cmd}' )
+        logger.info(f"sending: {cmd}")
 
-        cmd+="\n"
-        cmd=cmd.encode()
+        cmd += "\n"
+        cmd = cmd.encode()
 
         reply = vocto_io(sock, cmd)
 
@@ -110,77 +116,95 @@ def read_cmds(filename):
     Read vocto commands from a file.
     """
     with open(filename) as f:
-        lines=f.read().split('\n')
+        lines = f.read().split("\n")
         cmds = []
         for line in lines:
             line = line.strip()
             if not line:
                 # ignore blank lines.
                 continue
-            if line[0].startswith(("#",";")):
+            if line[0].startswith(("#", ";")):
                 # ignore #/; comment or whatever # lines
                 continue
             cmds.append(line)
 
-
     return cmds
+
 
 def log_setup(verbose):
 
-    levels = { 4:logging.CRITICAL, 3:logging.DEBUG, 2:logging.INFO, 1:logging.WARNING, 0:logging.ERROR }
+    levels = {
+        4: logging.CRITICAL,
+        3: logging.DEBUG,
+        2: logging.INFO,
+        1: logging.WARNING,
+        0: logging.ERROR,
+    }
     level = levels[verbose]
 
     logging.basicConfig(
-            format='{asctime} {funcName}:{lineno} {levelname}: {message}',
-            datefmt='%H:%M:%S',
-            level=level,
-            style="{")
-
+        format="{asctime} {funcName}:{lineno} {levelname}: {message}",
+        datefmt="%H:%M:%S",
+        level=level,
+        style="{",
+    )
 
 
 def get_args():
     parser = argparse.ArgumentParser(
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-            description='''Vocto-cmds Client
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description="""Vocto-cmds Client
             Sends commands to voctocore.
-            ''')
+            """,
+    )
 
     parser.add_argument(
-        '-c', '--cmds', action='append', default=[],
-        help="command to send to core.")
+        "-c", "--cmds", action="append", default=[], help="command to send to core."
+    )
+
+    parser.add_argument("-f", "--file", help="file of commands to send to core.")
 
     parser.add_argument(
-        '-f', '--file',
-        help="file of commands to send to core.")
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="print commands, replies and connection info.",
+    )
 
     parser.add_argument(
-        '-v', '--verbose', action='count', default=0,
-        help="print commands, replies and connection info.")
+        "--host", action="store", default="localhost", help="hostname of vocto core"
+    )
 
     parser.add_argument(
-        '--host', action='store',
-        default='localhost',
-        help="hostname of vocto core")
+        "--port", action="store", default=9999, help="port of vocto core"
+    )
 
     parser.add_argument(
-        '--port', action='store', default=9999,
-        help="port of vocto core")
+        "--timeout",
+        action="store",
+        type=float,
+        default=0.5,
+        help="seconds to wait for reply from server, then abort",
+    )
 
     parser.add_argument(
-        '--timeout', action='store', type=float, default=.5,
-        help="seconds to wait for reply from server, then abort")
+        "--delay",
+        action="store",
+        type=float,
+        default=0,
+        help="delay in seconds between commands",
+    )
 
     parser.add_argument(
-        '--delay', action='store', type=float, default=0,
-        help="delay in seconds between commands")
+        "--wait-for-core",
+        action="store_true",
+        help="loop forever when can't connect to core.",
+    )
 
     parser.add_argument(
-        '--wait-for-core', action='store_true',
-        help="loop forever when can't connect to core.")
-
-    parser.add_argument(
-        '--debug', action='store_true',
-        help="debugging things, (currently nothing)")
+        "--debug", action="store_true", help="debugging things, (currently nothing)"
+    )
 
     args = parser.parse_args()
 
@@ -203,5 +227,5 @@ def main():
     sys.exit()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
