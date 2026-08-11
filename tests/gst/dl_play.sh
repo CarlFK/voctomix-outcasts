28eed132b46dcccb17da9115347b0123a46ff913
Carl F. Karsten <carl@NextDayVideo.com>
2026-08-11 18:13:45 -0500

    decklink player

---
+++ tests/gst/dl_play.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -ex
+
+# dl_play.sh - sends decklinkvideosrc to screen, decklinkaudiosrc to speakers.
+
+gst-launch-1.0  \
+ -vv \
+ decklinkvideosrc device-number=0 mode=1080p30 \
+ ! queue \
+ ! clockoverlay \
+ ! queue \
+ ! videoconvert \
+ ! queue \
+ ! autovideosink \
+ decklinkaudiosrc device-number=0 \
+  ! queue \
+  ! volume volume=.2  \
+  ! autoaudiosink
+
